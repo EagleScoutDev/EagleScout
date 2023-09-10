b855c302c60a13e272adecd9e68be15b82e217b1
Gabor Szita <gaborszita88@gmail.com>
2023-09-10 14:58:52 -0700

    Fix register_user_with_team function

---
+++ supabase/migrations/20230910214423_fix_register_user_with_team_function.sql
@@ -0,0 +1,42 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.register_user_with_team(team_number integer)
+ RETURNS void
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO 'public'
+AS $function$
+DECLARE
+  existing_team_id INTEGER;
+  selected_team_id INTEGER;
+BEGIN
+  -- Check if the user is already registered with a team
+  SELECT team_id INTO existing_team_id
+  FROM public.user_attributes
+  WHERE id = auth.uid();
+
+  -- If the user is already registered with a team, raise an error
+  IF existing_team_id IS NOT NULL THEN
+    RAISE EXCEPTION 'User is already registered with a team';
+  END IF;
+
+  -- Get the team's id
+  IF EXISTS (
+    SELECT id
+    FROM public.teams
+    WHERE number = team_number
+  ) THEN
+    SELECT id INTO selected_team_id
+    FROM public.teams
+    WHERE number = team_number;
+  ELSE
+    RAISE EXCEPTION 'Team does not exist';
+  END IF;
+
+  -- Register the user with the specified team
+  UPDATE public.user_attributes SET (team_id, scouter, admin) = (team_id, FALSE, FALSE) WHERE id = auth.uid();
+END;
+$function$
+;
+
+
