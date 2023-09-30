00bf338fba891db68dbfddcae18973c41acd21de
Vir Shah <virn.shah@gmail.com>
2023-09-29 20:56:07 -0700

    Dismisses keyboard when pressed outside of keyboard

--- app/src/screens/login-flow/Login.js
+++ app/src/screens/login-flow/Login.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {
+  Keyboard,
   SafeAreaView,
   Text,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import {StyleSheet} from 'react-native';
@@ -53,102 +55,105 @@ const Login = ({onSubmit, error, ifAuth}) => {
   });
 
   return (
-    <SafeAreaView style={{flexDirection: 'column'}}>
-      <Text style={styles.titleText}>EagleScout</Text>
-      <View
-        style={{
-          backgroundColor: colors.card,
-          borderRadius: 10,
-          margin: '5%',
-          padding: '5%',
-          top: '15%',
-          borderWidth: 3,
-          borderColor: colors.primary,
-          shadowColor: colors.primary,
-          shadowOffset: {
-            width: 2,
-            height: 2,
-          },
-          shadowOpacity: 0.75,
-          shadowRadius: 3.84,
-        }}>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 20,
-            color: colors.text,
-            padding: 10,
-          }}>
-          Log In
-        </Text>
-        <TextInput
-          onChangeText={setUsername}
-          value={username}
-          placeholder="Email"
-          style={{
-            ...styles.input,
-            borderColor:
-              error === 'auth/missing-email' ||
-              error === 'auth/invalid-email' ||
-              error === 'auth/internal-error'
-                ? 'red'
-                : colors.text,
-          }}
-          inputMode={'email'}
-        />
-        <TextInput
-          onChangeText={setPassword}
-          value={password}
-          placeholder="Password"
-          style={{
-            ...styles.input,
-            borderColor:
-              error === 'auth/internal-error' || error === 'auth/wrong-password'
-                ? 'red'
-                : colors.text,
-          }}
-          secureTextEntry={true}
-        />
-        <StandardButton
-          text={'Log In'}
-          onPress={() => onSubmit(username, password)}
-          color={colors.primary}
-        />
-
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={{flexDirection: 'column'}}>
+        <Text style={styles.titleText}>EagleScout</Text>
         <View
           style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            padding: 20,
-          }}>
-          <Text style={{color: colors.text}}>Don't have an account?</Text>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('Sign');
-            }}>
-            <Text style={{color: colors.primary, fontWeight: 'bold'}}>
-              Register
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      {error !== '' && (
-        <View
-          style={{
-            backgroundColor: 'red',
-            padding: '5%',
-            margin: '3%',
+            backgroundColor: colors.card,
             borderRadius: 10,
-            position: 'absolute',
-            top: '130%',
-            justifyContent: 'center',
-            alignItems: 'center',
-            alignSelf: 'center',
+            margin: '5%',
+            padding: '5%',
+            top: '15%',
+            borderWidth: 3,
+            borderColor: colors.primary,
+            shadowColor: colors.primary,
+            shadowOffset: {
+              width: 2,
+              height: 2,
+            },
+            shadowOpacity: 0.75,
+            shadowRadius: 3.84,
           }}>
-          <Text style={{color: 'white', textAlign: 'center'}}>{error}</Text>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 20,
+              color: colors.text,
+              padding: 10,
+            }}>
+            Log In
+          </Text>
+          <TextInput
+            onChangeText={setUsername}
+            value={username}
+            placeholder="Email"
+            style={{
+              ...styles.input,
+              borderColor:
+                error === 'auth/missing-email' ||
+                error === 'auth/invalid-email' ||
+                error === 'auth/internal-error'
+                  ? 'red'
+                  : colors.text,
+            }}
+            inputMode={'email'}
+          />
+          <TextInput
+            onChangeText={setPassword}
+            value={password}
+            placeholder="Password"
+            style={{
+              ...styles.input,
+              borderColor:
+                error === 'auth/internal-error' ||
+                error === 'auth/wrong-password'
+                  ? 'red'
+                  : colors.text,
+            }}
+            secureTextEntry={true}
+          />
+          <StandardButton
+            text={'Log In'}
+            onPress={() => onSubmit(username, password)}
+            color={colors.primary}
+          />
+
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+              padding: 20,
+            }}>
+            <Text style={{color: colors.text}}>Don't have an account?</Text>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Sign');
+              }}>
+              <Text style={{color: colors.primary, fontWeight: 'bold'}}>
+                Register
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      )}
-    </SafeAreaView>
+        {error !== '' && (
+          <View
+            style={{
+              backgroundColor: 'red',
+              padding: '5%',
+              margin: '3%',
+              borderRadius: 10,
+              position: 'absolute',
+              top: '130%',
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'center',
+            }}>
+            <Text style={{color: 'white', textAlign: 'center'}}>{error}</Text>
+          </View>
+        )}
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
