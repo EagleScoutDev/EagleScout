cc0ae3e8f17cc9f0bf8a7cbc90aa48d91aed6418
Alan <alanlichen1@gmail.com>
2023-10-15 17:51:55 -0700

    report list collapsible, ordered per-competition

--- app/src/SVGIcons.js
+++ app/src/SVGIcons.js
@@ -131,3 +131,33 @@ export const CaretRight = () => {
     </Svg>
   );
 };
+
+export const ChevronDown = ({props}) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    {...props}>
+    <Path
+      fill-rule="evenodd"
+      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+    />
+  </Svg>
+);
+
+export const ChevronUp = ({props}) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    {...props}>
+    <Path
+      fill-rule="evenodd"
+      d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+    />
+  </Svg>
+);
--- app/src/components/ReportList.js
+++ app/src/components/ReportList.js
@@ -1,24 +1,240 @@
+import React from 'react';
 import {
-  View,
+  FlatList,
+  KeyboardAvoidingView,
+  LayoutAnimation,
+  Platform,
   Text,
   TouchableOpacity,
-  FlatList,
-  ScrollView,
-  KeyboardAvoidingView,
+  UIManager,
+  View,
 } from 'react-native';
 import {useEffect, useState} from 'react';
 import ScoutViewer from './modals/ScoutViewer';
 import {useTheme} from '@react-navigation/native';
+import {ChevronDown, ChevronUp} from '../SVGIcons';
+
+if (
+  Platform.OS === 'android' &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+function CompetitionFlatList({
+  compName,
+  data,
+  setChosenScoutForm,
+  setModalVisible,
+}) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [sort, setSort] = useState('');
+  const [dataCopy, setDataCopy] = useState(data);
+  const {colors} = useTheme();
+
+  const onPress = () => {
+    setIsCollapsed(!isCollapsed);
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+  };
+
+  const handleSort = (sortName, sortFunc) => {
+    if (sort === sortName) {
+      setSort('');
+      setDataCopy(data);
+    } else {
+      setSort(sortName);
+      setDataCopy(dataCopy.sort(sortFunc));
+    }
+  };
+
+  return (
+    <FlatList
+      data={data}
+      style={{
+        paddingBottom: 20,
+      }}
+      ListHeaderComponent={() => (
+        <View>
+          <TouchableOpacity onPress={onPress}>
+            <View
+              style={{
+                width: '90%',
+                alignSelf: 'center',
+                flexDirection: 'row',
+                paddingTop: 10,
+                paddingBottom: 20,
+                paddingHorizontal: 20,
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}>
+              <Text
+                style={{
+                  color: colors.text,
+                  fontSize: 30,
+                  fontWeight: 'bold',
+                }}>
+                {compName}
+              </Text>
+              {isCollapsed ? (
+                <ChevronUp width={30} height={30} />
+              ) : (
+                <ChevronDown width={30} height={30} />
+              )}
+            </View>
+          </TouchableOpacity>
+          <View
+            style={{
+              display: isCollapsed ? 'none' : 'flex',
+              width: '90%',
+              alignSelf: 'center',
+              borderRadius: 15,
+              paddingHorizontal: 20,
+              paddingBottom: 10,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 1.5,
+                borderRadius: 15,
+              }}
+              onPress={() =>
+                handleSort('team', (a, b) => a.teamNumber - b.teamNumber)
+              }>
+              <Text
+                style={{
+                  color: sort === 'team' ? colors.primary : colors.text,
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                }}>
+                Team
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+              }}
+              onPress={() =>
+                handleSort('match', (a, b) => a.matchNumber - b.matchNumber)
+              }>
+              <Text
+                style={{
+                  color: sort === 'match' ? colors.primary : colors.text,
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                }}>
+                Match
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 2,
+                borderRadius: 15,
+              }}
+              onPress={() =>
+                handleSort(
+                  'date',
+                  (a, b) => new Date(a.createdAt) - new Date(b.createdAt),
+                )
+              }>
+              <Text
+                style={{
+                  color: sort === 'date' ? colors.primary : colors.text,
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                }}>
+                Date
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+      renderItem={({item}) => (
+        <TouchableOpacity
+          onPress={() => {
+            setChosenScoutForm(item);
+            setModalVisible(true);
+          }}
+          style={{
+            display: isCollapsed ? 'none' : 'flex',
+          }}>
+          <View
+            style={{
+              display: item.isCollapsed ? 'none' : 'flex',
+              backgroundColor: colors.card,
+              width: '90%',
+              alignSelf: 'center',
+              borderRadius: 15,
+              padding: 20,
+              marginVertical: 5,
+              borderWidth: 1,
+              borderColor: colors.border,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Text
+              style={{
+                color: colors.text,
+                fontSize: 16,
+                fontWeight: 'bold',
+                flex: 1.5,
+              }}>
+              {item.teamNumber}
+            </Text>
+            <Text
+              style={{
+                color: colors.text,
+                fontSize: 16,
+                fontWeight: 'bold',
+                flex: 1,
+                textAlign: 'center',
+              }}>
+              {item.matchNumber}
+            </Text>
+            <Text
+              style={{
+                color: colors.text,
+                fontSize: 16,
+                fontWeight: 'bold',
+                flex: 2,
+                textAlign: 'right',
+              }}>
+              {new Date(item.createdAt).toLocaleDateString('en-US', {
+                month: 'short',
+                day: 'numeric',
+                year: 'numeric',
+              })}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      )}
+      // keyExtractor={item => item.id}
+    />
+  );
+}
 
 function ReportList({forms}) {
   const [modalVisible, setModalVisible] = useState(false);
   const [chosenScoutForm, setChosenScoutForm] = useState(null);
-  const [sort, setSort] = useState('');
   const [dataCopy, setDataCopy] = useState([]);
   const {colors} = useTheme();
 
   useEffect(() => {
-    setDataCopy(forms);
+    const comps = {};
+    for (const form of forms) {
+      if (!comps[form.competitionName]) {
+        comps[form.competitionName] = [];
+      }
+      comps[form.competitionName].push(form);
+    }
+    setDataCopy(
+      Object.keys(comps).map(comp => ({
+        title: comp,
+        data: comps[comp],
+      })),
+    );
   }, [forms]);
 
   if (forms == null) {
@@ -69,146 +285,15 @@ function ReportList({forms}) {
       <KeyboardAvoidingView behavior={'height'} style={{flex: 1}}>
         <FlatList
           data={dataCopy}
-          // add a header
-          ListHeaderComponent={() => (
-            <View
-              style={{
-                // backgroundColor: colors.card,
-                width: '90%',
-                alignSelf: 'center',
-                borderRadius: 15,
-                padding: 20,
-                // marginVertical: 10,
-                // borderWidth: 1,
-                // borderColor: colors.border,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 1.5,
-                  borderRadius: 15,
-                }}
-                onPress={() => {
-                  setSort('team');
-                  setDataCopy(
-                    dataCopy.sort((a, b) => {
-                      return a.team - b.team;
-                    }),
-                  );
-                }}>
-                <Text
-                  style={{
-                    color: sort === 'team' ? colors.primary : colors.text,
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                  }}>
-                  Team
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  flex: 1,
-                }}
-                onPress={() => {
-                  setSort('match');
-                  setDataCopy(
-                    dataCopy.sort((a, b) => {
-                      return a.match_number - b.match_umber;
-                    }),
-                  );
-                }}>
-                <Text
-                  style={{
-                    color: sort === 'match' ? colors.primary : colors.text,
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                    textAlign: 'center',
-                  }}>
-                  Match
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  flex: 2,
-                  borderRadius: 15,
-                }}
-                onPress={() => {
-                  setSort('date');
-                  setDataCopy(
-                    dataCopy.sort((a, b) => {
-                      return new Date(a.created_at) - new Date(b.created_at);
-                    }),
-                  );
-                }}>
-                <Text
-                  style={{
-                    color: sort === 'date' ? colors.primary : colors.text,
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                    textAlign: 'center',
-                  }}>
-                  Date
-                </Text>
-              </TouchableOpacity>
-            </View>
-          )}
           renderItem={({item, index, separators}) => (
-            <TouchableOpacity
-              onPress={() => {
-                setChosenScoutForm(item);
-                setModalVisible(true);
-              }}>
-              <View
-                style={{
-                  backgroundColor: colors.card,
-                  width: '90%',
-                  alignSelf: 'center',
-                  borderRadius: 15,
-                  padding: 20,
-                  marginVertical: 5,
-                  borderWidth: 1,
-                  borderColor: colors.border,
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}>
-                <Text
-                  style={{
-                    color: colors.text,
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                    flex: 1.5,
-                  }}>
-                  {item.teamNumber}
-                </Text>
-                <Text
-                  style={{
-                    color: colors.text,
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                    flex: 1,
-                    textAlign: 'center',
-                  }}>
-                  {item.matchNumber}
-                </Text>
-                <Text
-                  style={{
-                    color: colors.text,
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                    flex: 2,
-                    textAlign: 'right',
-                  }}>
-                  {new Date(item.createdAt).toLocaleDateString('en-US', {
-                    month: 'short',
-                    day: 'numeric',
-                    year: 'numeric',
-                  })}
-                </Text>
-              </View>
-            </TouchableOpacity>
+            <CompetitionFlatList
+              compName={item.title}
+              data={item.data}
+              setChosenScoutForm={setChosenScoutForm}
+              setModalVisible={setModalVisible}
+            />
           )}
-          // keyExtractor={item => item.id}
+          keyExtractor={item => item.index}
         />
       </KeyboardAvoidingView>
     );
