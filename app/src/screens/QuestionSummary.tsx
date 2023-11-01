2f7447cf07f55b649dff8b2058aa3855fb234b65
Vir Shah <virn.shah@gmail.com>
2023-11-01 14:44:57 -0700

    Shows average, max, and min values for number questions

--- app/src/screens/QuestionSummary.tsx
+++ app/src/screens/QuestionSummary.tsx
@@ -17,24 +17,37 @@ interface Props {
   ];
   generate_ai_summary: boolean;
 }
+
+interface Statistics {
+  average: number;
+  max: number;
+  min: number;
+}
+
 function QuestionSummary({item, index, data, generate_ai_summary}: Props) {
   const {colors} = useTheme();
   const [response, setResponse] = useState<String | null>('');
+  const [stats, setStats] = useState<Statistics | null>(null);
 
   useEffect(() => {
     if (item.type === 'textbox' && generate_ai_summary) {
-      // console.log('question: ' + item.question);
-      // console.log('question type: ' + item.type);
-
       let filteredData = data.filter(datum => datum.data !== '');
       let joinedData = filteredData.map(datum => datum.data).join('. ');
       OpenAI.getOpenAIResponse(item.question, joinedData).then(summary => {
         setResponse(summary);
       });
-      // console.log('summary: ' + summary);
-      // setResponse(summary);
     }
   }, [generate_ai_summary]);
+
+  useEffect(() => {
+    if (item.type === 'number') {
+      const avg =
+        data.map(datum => datum.data).reduce((a, b) => a + b, 0) / data.length;
+      const max = Math.max(...data.map(datum => datum.data));
+      const min = Math.min(...data.map(datum => datum.data));
+      setStats({average: avg, max: max, min: min});
+    }
+  }, []);
 
   if (item.type === 'heading') {
     return (
@@ -138,6 +151,43 @@ function QuestionSummary({item, index, data, generate_ai_summary}: Props) {
       <Text style={{color: 'green'}}>
         raw data - {data.map(datum => datum.data + ', ')}
       </Text>
+      {item.type == 'number' && (
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <View
+            style={{
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{color: colors.text, fontWeight: 'bold', fontSize: 25}}>
+              {stats ? stats.average.toFixed(2) : 'loading...'}
+            </Text>
+            <Text style={{color: 'gray', fontWeight: 'bold'}}>AVERAGE</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{color: colors.text, fontWeight: 'bold', fontSize: 25}}>
+              {stats ? stats.min : 'loading...'}
+            </Text>
+            <Text style={{color: 'gray', fontWeight: 'bold'}}>MINIMUM</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{color: colors.text, fontWeight: 'bold', fontSize: 25}}>
+              {stats ? stats.max : 'loading...'}
+            </Text>
+            <Text style={{color: 'gray', fontWeight: 'bold'}}>MAXIMUM</Text>
+          </View>
+        </View>
+      )}
       <Text style={{color: 'blue'}}>{response}</Text>
     </View>
   );
