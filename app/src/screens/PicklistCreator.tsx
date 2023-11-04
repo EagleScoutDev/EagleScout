aab8f9b37ffd7874f74251d30349463faf20c397
Vir Shah <virn.shah@gmail.com>
2023-11-04 00:51:36 -0700

    Adds dragging capability, structure to allow sharing

--- app/src/screens/PicklistCreator.tsx
+++ app/src/screens/PicklistCreator.tsx
@@ -1,63 +1,89 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
   StyleSheet,
-  FlatList,
   TextInput,
   Switch,
+  FlatList,
+  Pressable,
 } from 'react-native';
-
 import {useTheme} from '@react-navigation/native';
 import StandardButton from '../components/StandardButton';
 import PicklistsDB, {SimpleTeam} from '../database/Picklists';
 import StandardModal from '../components/modals/StandardModal';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import DraggableFlatList, {
+  RenderItemParams,
+  ScaleDecorator,
+} from 'react-native-draggable-flatlist';
+import Svg, {Path} from 'react-native-svg';
 
 function PicklistCreator() {
   const {colors} = useTheme();
 
-  const [name, setName] = React.useState(undefined);
-  const [possibleTeams, setPossibleTeams] = React.useState<SimpleTeam[]>([]);
+  const [name, setName] = useState<string | undefined>(undefined);
+  const [possibleTeams, setPossibleTeams] = useState<SimpleTeam[]>([]);
+  const [teamAddingModalVisible, setTeamAddingModalVisible] = useState(false);
+  const [filter_by_added, setFilterByAdded] = useState(false);
+  const [teams_list, setTeamsList] = useState<SimpleTeam[]>([]);
 
-  const [teamAddingModalVisible, setTeamAddingModalVisible] =
-    React.useState(true);
+  const [dragging_active, setDraggingActive] = useState(false);
 
-  // const [teams, setTeams] = React.useState<SimpleTeam[]>([]);
-  const [teams_list, setTeamsList] = React.useState<SimpleTeam[]>([]);
-
-  const [filter_by_added, setFilterByAdded] = React.useState(false);
+  const [shareModalVisible, setShareModalVisible] = useState(false);
 
   useEffect(() => {
-    PicklistsDB.getTeamsAtCompetition('2023cc').then(teams => {
-      setPossibleTeams(teams);
-    });
+    PicklistsDB.getTeamsAtCompetition('2023cc').then(setPossibleTeams);
   }, []);
 
-  function addTeam(team: SimpleTeam) {
-    if (teams_list.includes(team)) {
-      return;
-    }
-    setTeamsList([...teams_list, team]);
-  }
+  const renderItemDraggable = ({
+    item,
+    drag,
+    isActive,
+  }: RenderItemParams<SimpleTeam>) => {
+    return (
+      <ScaleDecorator>
+        <Pressable onPressIn={drag} style={styles.team_item_in_list}>
+          <Text style={{color: 'gray'}}>{teams_list.indexOf(item) + 1}</Text>
+          <Text
+            style={{
+              ...styles.team_number_displayed,
+              color: isActive ? 'blue' : colors.text,
+              fontWeight: isActive ? 'bold' : 'normal',
+            }}>
+            {item.team_number}
+          </Text>
+        </Pressable>
+      </ScaleDecorator>
+    );
+  };
 
-  function removeTeam(team: SimpleTeam) {
-    if (!teams_list.includes(team)) {
+  const renderItemStandard = ({item}) => {
+    return (
+      <Pressable style={styles.team_item_in_list}>
+        <Text style={{color: 'gray'}}>{teams_list.indexOf(item) + 1}</Text>
+        <Text style={styles.team_number_displayed}>{item.team_number}</Text>
+      </Pressable>
+    );
+  };
+
+  const addTeam = (team: SimpleTeam) => {
+    if (teams_list.some(t => t.team_number === team.team_number)) {
       return;
     }
-    setTeamsList(teams_list.filter(t => t !== team));
-  }
+    setTeamsList(prevTeams => [...prevTeams, team]);
+  };
+
+  const removeTeam = (team: SimpleTeam) => {
+    setTeamsList(prevTeams =>
+      prevTeams.filter(t => t.team_number !== team.team_number),
+    );
+  };
 
   const styles = StyleSheet.create({
     name_input: {
-      height: '20%',
-      // borderColor: 'gray',
-      // borderWidth: 1,
       color: colors.text,
       fontSize: 30,
-      // monospaced font
-      // list all available fonts
-      // https://reactnative.dev/docs/text-style-props#fontfamily
       fontFamily: 'monospace',
       fontWeight: name ? 'normal' : '200',
     },
@@ -72,20 +98,34 @@ function PicklistCreator() {
       fontSize: 20,
       borderWidth: 1,
       borderColor: colors.text,
-      // take up the max width
       minWidth: '80%',
       textDecorationLine: 'none',
+    },
+    team_item_in_list: {
+      padding: '2%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginRight: '60%',
+    },
+    team_number_displayed: {
+      flex: 1,
+      color: colors.text,
+      fontSize: 20,
+      marginLeft: '5%',
     },
   });
 
   return (
     <View style={styles.container}>
-      <Text
-        style={{
-          color: colors.text,
-        }}>
-        PicklistCreator
-      </Text>
+      <Pressable onPress={() => setDraggingActive(!dragging_active)}>
+        <Text
+          style={{
+            color: dragging_active ? colors.primary : colors.text,
+            fontSize: 30,
+          }}>
+          {dragging_active ? 'Reordering Active' : 'Reordering Inactive'}
+        </Text>
+      </Pressable>
       <TextInput
         style={styles.name_input}
         onPressIn={() => setName('')}
@@ -95,16 +135,6 @@ function PicklistCreator() {
         value={name}
         defaultValue={'Enter Name'}
       />
-      {teams_list.map(team => {
-        return (
-          <Text
-            style={{
-              color: colors.text,
-            }}>
-            {team.team_number}
-          </Text>
-        );
-      })}
       <StandardModal title={'Add Teams'} visible={teamAddingModalVisible}>
         <StandardButton
           color={'red'}
@@ -156,20 +186,86 @@ function PicklistCreator() {
           }}
         />
       </StandardModal>
-      <StandardButton
-        color={'blue'}
-        text={'Add Team'}
-        onPress={() => {
-          setTeamAddingModalVisible(true);
-        }}
-      />
-      <StandardButton
-        color={'blue'}
-        text={'Clear'}
-        onPress={() => setTeamsList([])}
-      />
-      <StandardButton color={'blue'} text={'Save'} onPress={() => {}} />
-      <StandardButton color={'blue'} text={'Publish'} onPress={() => {}} />
+      <StandardModal title={'Share'} visible={shareModalVisible}>
+        <StandardButton
+          color={'red'}
+          text={'Cancel'}
+          onPress={() => setShareModalVisible(false)}
+        />
+        <StandardButton color={'blue'} text={'Save'} onPress={() => {}} />
+        <StandardButton
+          color={'blue'}
+          text={'Publish to Team'}
+          onPress={() => {}}
+        />
+      </StandardModal>
+
+      {dragging_active ? (
+        <DraggableFlatList
+          data={teams_list}
+          onDragEnd={({data}) => setTeamsList(data)}
+          keyExtractor={item => String(item.team_number)}
+          renderItem={renderItemDraggable}
+        />
+      ) : (
+        <FlatList
+          data={teams_list}
+          renderItem={renderItemStandard}
+          keyExtractor={item => String(item.team_number)}
+        />
+      )}
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          position: 'absolute',
+          bottom: 0,
+          margin: '5%',
+          width: '100%',
+        }}>
+        <Pressable
+          style={{
+            backgroundColor: colors.primary,
+            borderColor: colors.primary,
+            borderWidth: 1,
+            padding: '5%',
+            borderRadius: 10,
+            alignItems: 'center',
+            flex: 1,
+          }}
+          onPress={() => {
+            setTeamAddingModalVisible(true);
+          }}>
+          <Text style={{color: 'white', fontSize: 20}}>Add Team</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => setShareModalVisible(true)}
+          style={{
+            backgroundColor: 'black',
+            borderColor: colors.border,
+            borderWidth: 1,
+            flex: 0.2,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 10,
+            padding: '5%',
+            marginLeft: '5%',
+          }}>
+          <Svg width={'50%'} height="100%" viewBox="0 0 16 16">
+            <Path
+              fill="white"
+              d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"
+            />
+          </Svg>
+        </Pressable>
+      </View>
+      {/*<StandardButton*/}
+      {/*  color={'blue'}*/}
+      {/*  text={'Clear'}*/}
+      {/*  onPress={() => setTeamsList([])}*/}
+      {/*/>*/}
+      {/*<StandardButton color={'blue'} text={'Save'} onPress={() => {}} />*/}
+      {/*<StandardButton color={'blue'} text={'Publish'} onPress={() => {}} />*/}
     </View>
   );
 }
