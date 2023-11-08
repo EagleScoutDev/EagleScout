e0e9c4d6e19eb2347b0bff6707420545ad19d287
Gabor Szita <gaborszita88@gmail.com>
2023-11-08 12:03:04 -0800

    fix dotenv typescript

--- app/src/lib/supabase.ts
+++ app/src/lib/supabase.ts
@@ -1,9 +1,10 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {createClient} from '@supabase/supabase-js';
+import {SUPABASE_URL, SUPABASE_ANON_KEY} from '@env';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = SUPABASE_URL;
+const supabaseAnonKey = SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
