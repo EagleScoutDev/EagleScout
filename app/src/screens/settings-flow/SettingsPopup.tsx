6628e47314aac2f25b0657c29eac0df470214b2d
Vir Shah <virn.shah@gmail.com>
2023-12-22 08:05:33 -0800

    Set up settings popover (aka modal).

--- app/src/screens/settings-flow/SettingsPopup.tsx
+++ app/src/screens/settings-flow/SettingsPopup.tsx
@@ -1,17 +1,59 @@
 import React from 'react';
-import {View, Text, Modal} from 'react-native';
+import {View, Text, Modal, Pressable} from 'react-native';
+import ThemePicker from '../../components/pickers/ThemePicker';
+import ScoutingStylePicker from '../../components/pickers/ScoutingStylePicker';
+import {useTheme} from '@react-navigation/native';
+import MinimalSectionHeader from '../../components/MinimalSectionHeader';
+import StandardButton from '../../components/StandardButton';
 
 interface SettingsPopupProps {
   visible: boolean;
   setVisible: (arg0: boolean) => void;
+  setTheme: (arg0: string) => void;
+  setScoutingStyle: (arg0: string) => void;
+  navigation: ReactNavigation.RootParamList;
 }
 
-const SettingsPopup = ({visible, setVisible}: SettingsPopupProps) => {
+const SettingsPopup = ({
+  visible,
+  setVisible,
+  setTheme,
+  setScoutingStyle,
+  navigation,
+}: SettingsPopupProps) => {
+  const {colors} = useTheme();
   return (
     <Modal
       visible={visible}
-      presentationStyle={'overFullScreen'}
+      presentationStyle={'formSheet'}
       onDismiss={() => setVisible(false)}>
+      <Pressable onPress={() => setVisible(false)}>
+        <Text
+          style={{
+            color: colors.text,
+            fontSize: 24,
+            marginVertical: '5%',
+            paddingLeft: '5%',
+          }}>
+          Settings
+        </Text>
+      </Pressable>
+      <ThemePicker colors={colors} setTheme={setTheme} />
+      <ScoutingStylePicker
+        colors={colors}
+        setScoutingStyle={setScoutingStyle}
+      />
+      <MinimalSectionHeader title={'Dev Tools'} />
+      <StandardButton
+        color={'black'}
+        isLoading={false}
+        onPress={() => {
+          setVisible(false);
+          navigation.navigate('Debug Offline');
+        }}
+        text={'View Device Storage'}
+      />
+      <View style={{marginVertical: '10%'}} />
       <Text>This will be in the settings popup.</Text>
     </Modal>
   );
