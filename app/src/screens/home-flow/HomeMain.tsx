43a4a965535dc7cb268489c81ffef1830afcd343
Vir Shah <virn.shah@gmail.com>
2024-01-03 23:50:46 +0530

    Hides FAB to begin scouting on home page.

--- app/src/screens/home-flow/HomeMain.tsx
+++ app/src/screens/home-flow/HomeMain.tsx
@@ -58,21 +58,21 @@ const HomeMain = () => {
           Pit scouting is not yet available. Please check back later.
         </Text>
       </ScrollView>
-      <Pressable
-        style={styles.scout_button}
-        onPress={() => {
-          navigation.navigate('Scout Report');
-        }}>
-        <View style={{flexDirection: 'row'}}>
-          <Text style={styles.button_text}>Begin Scouting</Text>
-          <Svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <Path
-              fill="white"
-              d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"
-            />
-          </Svg>
-        </View>
-      </Pressable>
+      {/*<Pressable*/}
+      {/*  style={styles.scout_button}*/}
+      {/*  onPress={() => {*/}
+      {/*    navigation.navigate('Scout Report');*/}
+      {/*  }}>*/}
+      {/*  <View style={{flexDirection: 'row'}}>*/}
+      {/*    <Text style={styles.button_text}>Begin Scouting</Text>*/}
+      {/*    <Svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">*/}
+      {/*      <Path*/}
+      {/*        fill="white"*/}
+      {/*        d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"*/}
+      {/*      />*/}
+      {/*    </Svg>*/}
+      {/*  </View>*/}
+      {/*</Pressable>*/}
     </View>
   );
 };
