5e5880818734675b042e94ffc7605b68555a6005
Gabor Szita <gaborszita88@gmail.com>
2024-02-06 18:31:19 -0800

    Fix '+' button in bottom tab too large

--- app/src/App.js
+++ app/src/App.js
@@ -280,7 +280,7 @@ const MyStack = ({themePreference, setThemePreference}) => {
                 backgroundColor: colors.background,
               },
               tabBarIcon: ({color, size, focused}) => (
-                <Svg width={size * 2.5} height={size * 2.5} viewBox="0 0 16 16">
+                <Svg width={'80%'} height={'80%'} viewBox="0 0 16 16">
                   <Path
                     fill={colors.primary}
                     d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"
