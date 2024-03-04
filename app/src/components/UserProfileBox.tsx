2d06d373e0b30de78dfab3c1c9595573f4e9d480
virshah <virnshah@gmail.com>
2024-03-03 22:07:50 -0800

    Adds an animating gradient circle to the profile page.

--- app/src/components/UserProfileBox.tsx
+++ app/src/components/UserProfileBox.tsx
@@ -1,8 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Easing, StyleSheet, Text, View} from 'react-native';
 import {Path, Svg} from 'react-native-svg';
 import {useTheme} from '@react-navigation/native';
 import {StoredUser} from '../lib/StoredUser';
 import React from 'react';
+import LinearGradient from 'react-native-linear-gradient'; // or 'expo-linear-gradient'
+import GradientShimmer from 'react-native-gradient-shimmer';
 
 interface UserProfileBoxProps {
   user: StoredUser | null;
@@ -53,15 +55,30 @@ function UserProfileBox({user}: UserProfileBoxProps) {
 
   return (
     <View style={styles.container}>
-      <View style={{marginRight: '5%'}}>
-        <Svg width={25} height={25} viewBox="0 0 16 16">
-          <Path fill="gray" d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-          <Path
-            fill="gray"
-            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-          />
-        </Svg>
-      </View>
+      {/*<View style={{marginRight: '5%'}}>*/}
+      {/*<Svg width={25} height={25} viewBox="0 0 16 16">*/}
+      {/*  <Path fill="gray" d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />*/}
+      {/*  <Path*/}
+      {/*    fill="gray"*/}
+      {/*    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"*/}
+      {/*  />*/}
+      {/*</Svg>*/}
+      {/*</View>*/}
+      <GradientShimmer
+        LinearGradientComponent={LinearGradient}
+        backgroundColor="darkblue"
+        highlightColor="blue"
+        animating={true}
+        duration={4000}
+        easing={Easing.linear}
+        highlightWidth={150}
+        height={50}
+        width={50}
+        style={{
+          borderRadius: 100,
+          marginRight: '5%',
+        }}
+      />
       <View>
         <Text style={styles.name_text}>
           {user.first_name} {user.last_name}
