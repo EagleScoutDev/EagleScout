281b768d9de9605c8bdf2d4c0d6f9e51dd1530e1
virshah <virnshah@gmail.com>
2024-03-03 16:18:09 -0800

    SVG ui bug

--- app/src/screens/picklist-flow/PicklistCreator.tsx
+++ app/src/screens/picklist-flow/PicklistCreator.tsx
@@ -20,6 +20,7 @@ import DraggableFlatList, {
 } from 'react-native-draggable-flatlist';
 import Svg, {Path} from 'react-native-svg';
 import ProfilesDB from '../../database/Profiles';
+import CompetitionsDB from '../../database/Competitions';
 
 function PicklistCreator({route}: {route: {params: {picklist_id: number}}}) {
   const {colors} = useTheme();
@@ -109,11 +110,11 @@ function PicklistCreator({route}: {route: {params: {picklist_id: number}}}) {
               marginRight: '5%',
             }}>
             <Path
-              fill={colors.primary}
+              fill={'gray'}
               d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
             />
             <Path
-              fill={colors.primary}
+              fill={'gray'}
               d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"
             />
           </Svg>
