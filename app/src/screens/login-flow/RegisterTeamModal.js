0880d6e5f5d786802807d5c9d79adc8c94c26b19
virshah <virnshah@gmail.com>
2024-03-12 15:48:10 -0700

    Updates register team screen to the same standards.

--- app/src/screens/login-flow/RegisterTeamModal.js
+++ app/src/screens/login-flow/RegisterTeamModal.js
@@ -1,15 +1,19 @@
 import {
   Alert,
+  Keyboard,
+  SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import {supabase} from '../../lib/supabase';
 import React, {useState} from 'react';
 import {useTheme} from '@react-navigation/native';
 import RadioButtons from '../../components/form/RadioButtons';
+import StandardButton from '../../components/StandardButton';
 
 function InputLabel(props) {
   return (
@@ -36,32 +40,23 @@ const RegisterTeamModal = ({navigation}) => {
   const [email, setEmail] = useState('');
 
   const styles = StyleSheet.create({
-    label: {
-      fontSize: 16,
-      paddingTop: 10,
-    },
     input: {
-      // center text
-      textAlign: 'center',
-      // add padding
-      padding: 10,
-      // add circular border around input field
+      textAlign: 'left',
+      padding: '5%',
       borderRadius: 10,
-      // add border
-      borderWidth: 1,
-      borderColor: colors.text,
-      // add margin
+      borderBottomWidth: 1,
+      borderColor: 'gray',
       // margin: 10,
-      // add horizontal space
-      marginHorizontal: 30,
-      color: colors.text,
+      // marginHorizontal: 30,
+      color: 'white',
     },
     titleText: {
-      textAlign: 'center',
+      textAlign: 'left',
+      padding: '5%',
       fontSize: 30,
       fontWeight: 'bold',
-      color: colors.primary,
-      marginVertical: 20,
+      color: 'rgb(191, 219, 247)',
+      // marginVertical: 20,
     },
     button: {
       textAlign: 'center',
@@ -69,60 +64,63 @@ const RegisterTeamModal = ({navigation}) => {
       fontWeight: 'bold',
       color: 'red',
     },
+    link_container: {
+      flexDirection: 'row',
+      padding: '4%',
+      borderRadius: 20,
+    },
+    background: {
+      flexDirection: 'column',
+      backgroundColor: 'rgb(0,0,25)',
+      flex: 1,
+    },
+    error: {
+      backgroundColor: 'red',
+      padding: '5%',
+      margin: '3%',
+      borderRadius: 10,
+      position: 'absolute',
+      top: '5%',
+      right: '5%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignSelf: 'center',
+    },
+    error_text: {
+      color: 'white',
+      textAlign: 'center',
+    },
+    label: {
+      color: 'gray',
+      fontWeight: 'bold',
+      fontSize: 12,
+      paddingTop: 10,
+    },
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: colors.background,
-      }}>
-      <TouchableOpacity
-        style={{
-          position: 'absolute',
-          top: '8%',
-          left: '5%',
-        }}
-        onPress={() => {
-          navigation.goBack();
-        }}>
-        <Text
-          style={{
-            ...styles.button,
-            color: colors.notification,
-            fontSize: 20,
-          }}>
-          {/*TODO: Figure out the elegant react navigation method for doing this.*/}
-          {'< Return Back'}
-        </Text>
-      </TouchableOpacity>
-      <View
-        style={{
-          backgroundColor: colors.card,
-          borderRadius: 10,
-          margin: '5%',
-          padding: '5%',
-          top: '13%',
-          borderWidth: 3,
-          borderColor: colors.primary,
-          // add drop shadow
-          shadowColor: colors.primary,
-          shadowOffset: {
-            width: 2,
-            height: 2,
-          },
-          shadowOpacity: 0.75,
-          shadowRadius: 3.84,
-        }}>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 20,
-            color: colors.text,
-            padding: 10,
-          }}>
-          Register your team with EagleScout
-        </Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      {/*<TouchableOpacity*/}
+      {/*  style={{*/}
+      {/*    position: 'absolute',*/}
+      {/*    top: '8%',*/}
+      {/*    left: '5%',*/}
+      {/*  }}*/}
+      {/*  onPress={() => {*/}
+      {/*    navigation.goBack();*/}
+      {/*  }}>*/}
+      {/*  <Text*/}
+      {/*    style={{*/}
+      {/*      ...styles.button,*/}
+      {/*      color: colors.notification,*/}
+      {/*      fontSize: 20,*/}
+      {/*    }}>*/}
+      {/*    /!*TODO: Figure out the elegant react navigation method for doing this.*!/*/}
+      {/*    {'< Return Back'}*/}
+      {/*  </Text>*/}
+      {/*</TouchableOpacity>*/}
+      <SafeAreaView style={styles.background}>
+        <Text style={styles.titleText}>Register your team with EagleScout</Text>
         <View>
           <Text style={styles.label}>
             Are you registering an actual FRC team?
@@ -134,7 +132,7 @@ const RegisterTeamModal = ({navigation}) => {
             ]}
             value={teamOption}
             onValueChange={setTeamOption}
-            colors={colors}
+            colors={{text: 'white'}}
           />
           {teamOption != null && teamOption.substring(0, 1) === 'Y' && (
             <>
@@ -167,14 +165,17 @@ const RegisterTeamModal = ({navigation}) => {
             inputMode={'email'}
           />
         </View>
-        <TouchableOpacity
-          style={{
-            backgroundColor: colors.primary,
-            padding: 10,
-            borderRadius: 10,
-            margin: 10,
-            marginHorizontal: 30,
-          }}
+        <StandardButton
+          text={'Register'}
+          textColor={
+            email === '' ||
+            (teamOption != null &&
+              teamOption.substring(0, 1) === 'Y' &&
+              team === '')
+              ? 'red'
+              : colors.primary
+          }
+          disabled={false}
           onPress={async () => {
             if (teamOption == null) {
               Alert.alert(
@@ -216,13 +217,25 @@ const RegisterTeamModal = ({navigation}) => {
               );
               navigation.navigate('Login');
             }
+          }}
+        />
+
+        <TouchableOpacity
+          style={styles.link_container}
+          onPress={() => {
+            navigation.navigate('Login');
           }}>
-          <Text style={{textAlign: 'center', color: 'white', fontSize: 20}}>
-            Register your team
-          </Text>
+          <Text style={{color: 'gray'}}>Log In</Text>
         </TouchableOpacity>
-      </View>
-    </View>
+        <TouchableOpacity
+          style={styles.link_container}
+          onPress={() => {
+            navigation.navigate('Sign');
+          }}>
+          <Text style={{color: 'gray'}}>Create Account</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
