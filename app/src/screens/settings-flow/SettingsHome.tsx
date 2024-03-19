5b4a983dadd77974cf7c621b958b8a68c8a6f68e
virshah <virnshah@gmail.com>
2024-03-18 23:36:21 -0700

    Increments version number

--- app/src/screens/settings-flow/SettingsHome.tsx
+++ app/src/screens/settings-flow/SettingsHome.tsx
@@ -21,7 +21,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {StoredUser} from '../../lib/StoredUser';
 import Competitions from '../../database/Competitions';
 
-const VERSION = '7.1.1 (OTA 3)';
+const VERSION = '7.2';
 
 interface SettingsHomeProps {
   onSignOut: () => void;
