fbe7568f7666c1eb7a18c79a2ffbd38b8749ef14
virshah <virnshah@gmail.com>
2024-05-08 17:46:35 -0700

    Gives minimal section header a boost if the background is too dark

--- app/src/components/MinimalSectionHeader.js
+++ app/src/components/MinimalSectionHeader.js
@@ -1,11 +1,14 @@
 import React, {Text} from 'react-native';
+import {isColorDark} from '../lib/ColorReadability';
+import {useTheme} from '@react-navigation/native';
 
 function MinimalSectionHeader(props) {
+  const {colors} = useTheme();
   return (
     <Text
       // eslint-disable-next-line react-native/no-inline-styles
       style={{
-        color: 'gray',
+        color: isColorDark(colors.background) ? 'gray' : 'lightgray',
         fontWeight: 'bold',
         fontSize: 12,
         paddingLeft: '2%',
--- app/src/lib/ColorReadability.ts
+++ app/src/lib/ColorReadability.ts
@@ -28,3 +28,21 @@ export const getIdealTextColorFromRGB = (bgColor: string) => {
   // Determine if the text color should be black or white based on the luminance
   return 255 - bgDelta < nThreshold ? '#000000' : '#ffffff';
 };
+
+export const isColorDark = (bgColor: string) => {
+  const nThreshold = 110;
+  // Extract the numeric values from the RGB string
+  const rgbValues = bgColor.match(/\d+/g).map(Number);
+
+  const components = {
+    R: rgbValues[0],
+    G: rgbValues[1],
+    B: rgbValues[2],
+  };
+
+  // Calculate the luminance of the background color
+  const bgDelta =
+    components.R * 0.299 + components.G * 0.587 + components.B * 0.114;
+
+  return 255 - bgDelta < nThreshold;
+};
