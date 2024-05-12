660ea506040cf92816c08d552b948177eb3ee891
virshah <virnshah@gmail.com>
2024-05-11 22:45:24 -0700

    Shrinks theme picker so other settings are visible

--- app/src/components/pickers/ThemePicker.tsx
+++ app/src/components/pickers/ThemePicker.tsx
@@ -35,76 +35,70 @@ function ThemePicker({setTheme}: {setTheme: (arg0: ThemeOptions) => void}) {
   return (
     <View>
       <MinimalSectionHeader title="Theme" />
-      <View
+      <ScrollView
         style={{
-          flexDirection: 'row',
           margin: 20,
           padding: 2,
           borderRadius: 10,
           borderWidth: 2,
           borderColor: colors.border,
           alignContent: 'center',
+          height: '36%',
         }}>
-        <ScrollView
-          style={{
-            flex: 1,
-          }}>
-          {Array.from(ThemeOptionsMap.keys()).map(themeOption => {
-            return (
-              <Pressable
-                onPress={() => {
-                  setSelectedTheme(themeOption);
-                  setTheme(themeOption);
-                  saveThemePreference(themeOption);
-                }}
-                key={themeOption}
-                style={{
-                  flexDirection: 'row',
-                  flex: 1,
-                  borderWidth: 2, //themeOption === selectedTheme ? 2 : 0,
-                  borderRadius: 10,
-                  borderColor:
-                    themeOption === selectedTheme ? colors.text : colors.card,
-                  // borderRadius: 10,
-                  padding: 10,
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  backgroundColor:
-                    themeOption === selectedTheme
-                      ? colors.background
-                      : colors.card,
-                }}>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                  <Text
-                    style={{
-                      color: colors.text,
-                      flex: 1,
-                      fontWeight: 'bold',
-                      fontSize: 16,
-                    }}>
-                    {ThemeOptions[themeOption]
-                      .toLowerCase()
-                      .charAt(0)
-                      .toUpperCase() +
-                      ThemeOptions[themeOption].toLowerCase().slice(1)}
-                  </Text>
+        {Array.from(ThemeOptionsMap.keys()).map(themeOption => {
+          return (
+            <Pressable
+              onPress={() => {
+                setSelectedTheme(themeOption);
+                setTheme(themeOption);
+                saveThemePreference(themeOption);
+              }}
+              key={themeOption}
+              style={{
+                flexDirection: 'row',
+                flex: 1,
+                borderWidth: 2, //themeOption === selectedTheme ? 2 : 0,
+                borderRadius: 10,
+                borderColor:
+                  themeOption === selectedTheme ? colors.text : colors.card,
+                // borderRadius: 10,
+                padding: 10,
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                backgroundColor:
+                  themeOption === selectedTheme
+                    ? colors.background
+                    : colors.card,
+              }}>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Text
+                  style={{
+                    color: colors.text,
+                    flex: 1,
+                    fontWeight: 'bold',
+                    fontSize: 16,
+                  }}>
+                  {ThemeOptions[themeOption]
+                    .toLowerCase()
+                    .charAt(0)
+                    .toUpperCase() +
+                    ThemeOptions[themeOption].toLowerCase().slice(1)}
+                </Text>
 
-                  <View
-                    style={{
-                      // flex: 1,
-                      borderRadius: 200,
-                      width: 40,
-                      height: 40,
-                      backgroundColor:
-                        ThemeOptionsMap.get(themeOption)?.colors.background,
-                    }}
-                  />
-                </View>
-              </Pressable>
-            );
-          })}
-        </ScrollView>
-      </View>
+                <View
+                  style={{
+                    borderRadius: 200,
+                    width: 40,
+                    height: 40,
+                    backgroundColor:
+                      ThemeOptionsMap.get(themeOption)?.colors.background,
+                  }}
+                />
+              </View>
+            </Pressable>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
