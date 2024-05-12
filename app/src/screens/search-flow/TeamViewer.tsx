fbb1085bee4d874109fa49b4d0681521ee5cf7c8
virshah <virnshah@gmail.com>
2024-05-11 23:40:36 -0700

    Allows user to create 'performance graphs' for any team -- a graph with several streams of scouting question data overlaid on top of each other.

--- app/src/screens/search-flow/TeamViewer.tsx
+++ app/src/screens/search-flow/TeamViewer.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -11,7 +11,12 @@ import {SimpleEvent, SimpleTeam, TBA} from '../../lib/TBAUtils';
 import Statbotics from '../../components/Statbotics';
 import CompetitionRank from './CompetitionRank';
 import ScoutSummary from './ScoutSummary';
-import {CaretRight} from '../../SVGIcons'; // adjust the import path to match your file structur
+import {CaretRight} from '../../SVGIcons';
+import ListItemContainer from '../../components/ListItemContainer';
+import ListItem from '../../components/ListItem';
+import Svg, {Path} from 'react-native-svg';
+import QuestionFormulaCreator from '../data-flow/QuestionFormulaCreator';
+import CombinedGraph from './CombinedGraph'; // adjust the import path to match your file structur
 
 interface TeamViewerProps {
   team: SimpleTeam;
@@ -20,8 +25,16 @@ interface TeamViewerProps {
 
 const TeamViewer: React.FC<TeamViewerProps> = ({route}) => {
   const {colors} = useTheme();
+  // team is a SimpleTeam
   const {team, competitionId} = route.params;
   const navigation = useNavigation();
+
+  const [graphCreationModalVisible, setGraphCreationModalVisible] =
+    useState(false);
+  const [chosenQuestionIndices, setChosenQuestionIndices] = useState<number[]>(
+    [],
+  );
+  const [graphActive, setGraphActive] = useState(false);
 
   const styles = StyleSheet.create({
     team_header: {
@@ -57,6 +70,12 @@ const TeamViewer: React.FC<TeamViewerProps> = ({route}) => {
     },
   });
 
+  useEffect(() => {
+    if (chosenQuestionIndices.length > 0 && !graphCreationModalVisible) {
+      setGraphActive(true);
+    }
+  }, [chosenQuestionIndices, graphCreationModalVisible]);
+
   return (
     <View>
       <ScrollView>
@@ -65,17 +84,53 @@ const TeamViewer: React.FC<TeamViewerProps> = ({route}) => {
 
         <CompetitionRank team_number={team.team_number} />
 
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('Reports for Team', {
-              team_number: team.team_number,
-              competitionId: competitionId,
-            });
-          }}
-          style={styles.reports_button}>
-          <Text style={styles.label}>See all scouting reports and notes</Text>
-          {CaretRight()}
-        </TouchableOpacity>
+        <ListItemContainer title={'Team Stats'}>
+          <ListItem
+            text={'See all scouting reports and notes'}
+            onPress={() => {
+              navigation.navigate('Reports for Team', {
+                team_number: team.team_number,
+                competitionId: competitionId,
+              });
+            }}
+            caretVisible={true}
+            disabled={false}
+            icon={() => null}
+          />
+          <ListItem
+            text={'Create Performance Graph'}
+            onPress={() => setGraphCreationModalVisible(true)}
+            caretVisible={false}
+            disabled={false}
+            icon={() => {
+              return (
+                <Svg
+                  width="16"
+                  height="16"
+                  fill={colors.text}
+                  viewBox="0 0 16 16">
+                  <Path d="M0 0h1v15h15v1H0zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07" />
+                </Svg>
+              );
+            }}
+          />
+        </ListItemContainer>
+
+        <QuestionFormulaCreator
+          visible={graphCreationModalVisible}
+          setVisible={setGraphCreationModalVisible}
+          chosenQuestionIndices={chosenQuestionIndices}
+          setChosenQuestionIndices={setChosenQuestionIndices}
+          compId={competitionId}
+        />
+
+        <CombinedGraph
+          team_number={team.team_number}
+          competitionId={competitionId}
+          modalActive={graphActive}
+          setModalActive={setGraphActive}
+          questionIndices={chosenQuestionIndices}
+        />
 
         <Statbotics team={team.team_number} />
         <ScoutSummary
