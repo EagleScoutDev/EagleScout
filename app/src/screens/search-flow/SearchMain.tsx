deae762199465f6488d6f170e75096775d379a85
virshah <virnshah@gmail.com>
2024-05-12 00:53:02 -0700

    Hides magnifying glass until competition has finished loading.

--- app/src/screens/search-flow/SearchMain.tsx
+++ app/src/screens/search-flow/SearchMain.tsx
@@ -178,27 +178,29 @@ const SearchMain: React.FC<Props> = ({navigation}) => {
           {/*    />*/}
           {/*  </Svg>*/}
           {/*</Pressable>*/}
-          <Pressable
-            style={{
-              paddingHorizontal: '8%',
-              paddingVertical: '4%',
-              // backgroundColor: colors.card,
-            }}
-            onPress={() => {
-              navigation.navigate('SearchModal', {
-                teams: listOfTeams,
-                reportsByMatch: reportsByMatch,
-                competitionId: competitionId,
-              });
-            }}>
-            <Svg width={'20'} height="20" viewBox="0 0 16 16">
-              <Path
-                fill={colors.text}
-                opacity={0.7}
-                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-              />
-            </Svg>
-          </Pressable>
+          {!fetchingData && (
+            <Pressable
+              style={{
+                paddingHorizontal: '8%',
+                paddingVertical: '4%',
+                // backgroundColor: colors.card,
+              }}
+              onPress={() => {
+                navigation.navigate('SearchModal', {
+                  teams: listOfTeams,
+                  reportsByMatch: reportsByMatch,
+                  competitionId: competitionId,
+                });
+              }}>
+              <Svg width={'20'} height="20" viewBox="0 0 16 16">
+                <Path
+                  fill={colors.text}
+                  opacity={0.7}
+                  d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+                />
+              </Svg>
+            </Pressable>
+          )}
         </View>
         <View
           style={{
