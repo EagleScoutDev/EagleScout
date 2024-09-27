68b11feaf7fc03c5503f2eb01ae0657d38dbd502
Alan <alanlichen1@gmail.com>
2024-09-26 23:34:13 -0700

    Add icon to auto path viewer

--- app/src/screens/search-flow/TeamViewer.tsx
+++ app/src/screens/search-flow/TeamViewer.tsx
@@ -109,21 +109,28 @@ const TeamViewer: React.FC<TeamViewerProps> = ({route}) => {
               );
             }}
           />
-		<ListItem
-			text={'See auto paths'}
-			onPress={() => {
-				navigation.navigate('Auto Paths', {
-					team_number: team.team_number,
-					competitionId: competitionId,
-				});
-			}}
-			caretVisible={true}
-			disabled={false}
-			icon={() => {
-				return (
-				);
-			}}
-		/>
+          <ListItem
+            text={'See auto paths'}
+            onPress={() => {
+              navigation.navigate('Auto Paths', {
+                team_number: team.team_number,
+                competitionId: competitionId,
+              });
+            }}
+            caretVisible={true}
+            disabled={false}
+            icon={() => {
+              return (
+                <Svg
+                  width="16"
+                  height="16"
+                  fill={colors.primary}
+                  viewBox="0 0 256 256">
+                  <Path d="M200,164a36.07,36.07,0,0,0-33.94,24H72a28,28,0,0,1,0-56h96a44,44,0,0,0,0-88H72a12,12,0,0,0,0,24h96a20,20,0,0,1,0,40H72a52,52,0,0,0,0,104h94.06A36,36,0,1,0,200,164Zm0,48a12,12,0,1,1,12-12A12,12,0,0,1,200,212Z" />
+                </Svg>
+              );
+            }}
+          />
           <ListItem
             text={'Create Performance Graph'}
             onPress={() => setGraphCreationModalVisible(true)}
