5e6a57ece13a3f6a75f5c7b7ab012cfafdac1269
Alan <alanlichen1@gmail.com>
2024-11-05 14:59:39 -0800

    Increment version to 7.6.1

--- app/src/screens/settings-flow/SettingsHome.tsx
+++ app/src/screens/settings-flow/SettingsHome.tsx
@@ -25,7 +25,7 @@ import {ThemeOptions} from '../../themes/ThemeOptions';
 import {getLighterColor} from '../../lib/ColorReadability';
 import TabHeader from '../../components/TabHeader';
 
-const VERSION = '7.4 (OTA 2.5A)';
+const VERSION = '7.6.1';
 
 interface SettingsHomeProps {
   onSignOut: () => void;
