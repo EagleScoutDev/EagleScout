dc03809d74e78ed9c9853dbe5e37265b14a680d2
maxicode2 <65052855+maxicode2@users.noreply.github.com>
2025-11-14 20:27:09 -0800

    Update UICheckboxes.tsx

--- app/src/ui/input/UICheckboxes.tsx
+++ app/src/ui/input/UICheckboxes.tsx
@@ -8,15 +8,20 @@ export interface UICheckboxesProps<T extends string> {
 }
 export function UICheckboxes<T extends string>({ options, value, onInput }: UICheckboxesProps<T>) {
     "use memo";
+    console.log(value)
+
     return (
-        <View>
+        <View style={{ gap: 8 }}>
             {options.map((item, index) => (
                 <UICheckbox
+                    key={index}
+                    text={item}
                     value={value.includes(item)}
                     onInput={(x) => {
                         if (!onInput) return;
 
-                        if (x) {
+                        if (!x) {
+                            console.log(value.includes(item) ? value : [...value, item])
                             onInput(value.includes(item) ? value : [...value, item]);
                         } else {
                             onInput(value.filter((x) => x !== item));
