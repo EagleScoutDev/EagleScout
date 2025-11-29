e666c6313bafcbe1f2c798a836cd23ea4b0e9896
maxicode2 <65052855+maxicode2@users.noreply.github.com>
2025-11-28 20:04:25 -0800

    Simplify the statbotics summary component

--- app/src/components/StatboticsSummary.tsx
+++ app/src/components/StatboticsSummary.tsx
@@ -1,116 +1,76 @@
-import { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { type PropsWithChildren, useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import { Statbotics } from "../lib/frc/statbotics";
 
 // Note: Statbotics is said to update their data every 6 hours from Blue Alliance.
 
-/**
- * A capsule that displays a value and a label.
- * @param title {string} The label to display.
- * @param value {string} The value to display.
- * @returns {JSX.Element} The capsule.
- * @constructor The capsule.
- */
-function InfoCapsule({ title, value, textColor }) {
+interface InfoCapsuleProps {
+    title: string;
+    value: number | null;
+}
+function InfoCapsule({ title, value }: InfoCapsuleProps) {
+    const { colors } = useTheme();
+
     return (
-        <View style={{ flexDirection: "column", alignItems: "center" }}>
-            <Text style={{ fontSize: 20, fontWeight: "bold", color: textColor }}>{value}</Text>
+        <View style={{ flex: 1, alignItems: "center" }}>
+            <Text style={{ fontSize: 20, fontWeight: "bold", color: colors.text }}>{value ?? "N/A"}</Text>
             <Text style={{ fontSize: 12, color: "gray" }}>{title}</Text>
         </View>
     );
 }
 
-/**
- * A row of capsules.
- * @param children {JSX.Element[]} The capsules to display.
- * @returns {JSX.Element} The row.
- * @constructor The row.
- */
-function InfoRow({ children }) {
-    return (
-        <View
-            style={{
-                flexDirection: "row",
-                justifyContent: "space-around",
-                padding: 15,
-            }}
-        >
-            {children}
-        </View>
-    );
+function InfoRow({ children }: PropsWithChildren) {
+    return <View style={{ flexDirection: "row", justifyContent: "space-around" }}>{children}</View>;
 }
 
-export function StatboticsSummary({ team }) {
-    const [overall, setOverall] = useState<Statbotics.TeamYear | null>(null);
-    const [competitions, setCompetitions] = useState<Statbotics.TeamEvent[] | null>(null);
-    const [visible, setVisible] = useState(true);
-    const [isTeam, setIsTeam] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isTeamValid, setIsTeamValid] = useState(false);
+export interface StatboticsSummaryProps {
+    team: number;
+}
+export function StatboticsSummary({ team }: StatboticsSummaryProps) {
+    "use memo";
+
     const { colors } = useTheme();
 
-    /**
-     * Fetches a team's overall data from Statbotics.
-     * @param team_num {string} The team number to fetch data for.
-     * @returns {Promise<void>} A promise that resolves when the data is fetched.
-     */
-    async function fetchTeam() {
-        setIsLoading(true);
-        const teamData = await Statbotics.getTeamYear(team);
-        if (teamData) {
-            setOverall(teamData);
-            setIsTeam(true);
-        } else {
-            setIsTeam(false);
-        }
-        setIsLoading(false);
-    }
+    const [overall, setOverall] = useState<Statbotics.TeamYear | null>(null);
+    const [competitions, setCompetitions] = useState<Statbotics.TeamEvent[] | null>(null);
+    const [loading, setLoading] = useState(false);
 
-    /**
-     * Fetches the team's event data from Statbotics.
-     * This includes every event that the team participated in.
-     */
-    async function fetchTeamEvent() {
-        const teamEvents = await Statbotics.getTeamEvents(team);
-        if (teamEvents) {
-            setCompetitions(teamEvents);
-        } else {
-        }
+    async function refresh() {
+        setLoading(true);
+        await Promise.all([
+            Statbotics.getTeamYear(team).then(setOverall),
+            Statbotics.getTeamEvents(team).then(setCompetitions),
+        ]);
+        setLoading(false);
     }
 
     useEffect(() => {
         setOverall(null);
-        // setCompetitions(null);
-
-        if (/^\d+$/.test(team) && Number(team) < 100000) {
-            setIsTeamValid(true);
-            fetchTeam(team).then(() => {
-                fetchTeamEvent();
-            });
-        } else {
-            setIsTeamValid(false);
-        }
-        //eslint-disable-next-line react-hooks/exhaustive-deps
+        void refresh();
     }, [team]);
 
     const styles = StyleSheet.create({
-        formSection: {
+        container: {
             width: "100%",
-            flexDirection: "column",
             padding: 16,
             backgroundColor: colors.card,
             borderWidth: 1,
             borderColor: colors.border,
             borderRadius: 10,
-            alignSelf: "center",
         },
     });
 
-    // empty team number
-    if (team === "") {
+    let title = `Team #${team} Statistics`;
+    let message: string | undefined;
+
+    if (loading) message = "Loading...";
+    else if (overall === null || competitions === null)
+        message = "This team is not in the database. Please check your team number.";
+
+    if (message !== undefined) {
         return (
-            <View style={styles.formSection}>
+            <View style={styles.container}>
                 <Text
                     style={{
                         textAlign: "center",
@@ -118,9 +78,8 @@ export function StatboticsSummary({ team }) {
                         fontSize: 20,
                         color: colors.text,
                     }}
-                    onPress={() => setVisible(!visible)}
                 >
-                    Team Statistics
+                    {title}
                 </Text>
                 <Text
                     style={{
@@ -129,188 +88,69 @@ export function StatboticsSummary({ team }) {
                         color: colors.text,
                     }}
                 >
-                    Get started by entering a team number.
+                    {message}
                 </Text>
             </View>
         );
-    }
+    } else {
+        if (overall === null) {
+            throw new Error("Assertion failed"); // message should have been set to true if overall === null
+        }
 
-    if (!isTeamValid) {
         return (
-            <View style={styles.formSection}>
-                <Text
-                    style={{
-                        textAlign: "center",
-                        fontWeight: "bold",
-                        fontSize: 20,
-                        color: colors.text,
-                    }}
-                    onPress={() => setVisible(!visible)}
-                >
-                    Team Statistics
-                </Text>
-                <Text
-                    style={{
-                        textAlign: "center",
-                        fontStyle: "italic",
-                        color: colors.text,
-                    }}
-                >
-                    Please enter a valid team number.
-                </Text>
-            </View>
-        );
-    }
-
-    // team number is not in the database
-    if (isLoading) {
-        return (
-            <View style={styles.formSection}>
-                <Text
-                    style={{
-                        textAlign: "center",
-                        fontWeight: "bold",
-                        fontSize: 20,
-                        color: colors.text,
-                    }}
-                    onPress={() => setVisible(!visible)}
-                >
-                    Team {team} Statistics
-                </Text>
-                <Text
-                    style={{
-                        textAlign: "center",
-                        fontStyle: "italic",
-                        color: colors.text,
-                    }}
-                >
-                    Loading...
-                </Text>
-            </View>
-        );
-    }
-
-    // team number is not in the database
-    if (!isTeam) {
-        return (
-            <View style={styles.formSection}>
-                <Text
-                    style={{
-                        textAlign: "center",
-                        fontWeight: "bold",
-                        fontSize: 20,
-                        color: colors.text,
-                    }}
-                    onPress={() => setVisible(!visible)}
-                >
-                    Team {team} Statistics
-                </Text>
-                <Text style={{ textAlign: "center", color: "red" }}>
-                    This team is not in the database. Please check your team number.
-                </Text>
-            </View>
-        );
-    }
-
-    // minimized view, only shows team number and nickname
-    if (!visible) {
-        return (
-            <View style={styles.formSection}>
-                <Text
-                    style={{
-                        textAlign: "center",
-                        fontWeight: "bold",
-                        fontSize: 20,
-                        color: colors.text,
-                    }}
-                    onPress={() => setVisible(!visible)}
-                >
-                    Team {team} Statistics
-                </Text>
-                <Text
-                    style={{
-                        textAlign: "center",
-                        fontStyle: "italic",
-                        color: colors.text,
-                    }}
-                >
-                    {overall ? overall.name : "Loading..."}
-                </Text>
-            </View>
-        );
-    }
-
-    return (
-        <View style={styles.formSection}>
-            <View>
-                <Text
-                    style={{
-                        fontWeight: "bold",
-                        fontSize: 15,
-                        color: colors.text,
-                    }}
-                >
-                    Overall Statistics
-                </Text>
-                {overall && "epa" in overall && (
-                    <InfoRow>
-                        <InfoCapsule
-                            title="Auto EPA"
-                            value={overall.epa.breakdown.auto_points}
-                            textColor={colors.text}
-                        />
-                        <InfoCapsule
-                            title="Teleop EPA"
-                            value={overall.epa.breakdown.teleop_points}
-                            textColor={colors.text}
-                        />
-                        <InfoCapsule
-                            title="Endgame EPA"
-                            value={overall.epa.breakdown.endgame_points}
-                            textColor={colors.text}
-                        />
-                    </InfoRow>
+            <View style={styles.container}>
+                {overall.epa.breakdown && (
+                    <>
+                        <Text
+                            style={{
+                                fontWeight: "bold",
+                                fontSize: 15,
+                                color: colors.text,
+                            }}
+                        >
+                            Overall Statistics
+                        </Text>
+                        <InfoRow>
+                            <InfoCapsule title="Auto EPA" value={overall.epa.breakdown.auto_points} />
+                            <InfoCapsule title="Teleop EPA" value={overall.epa.breakdown.teleop_points} />
+                            <InfoCapsule title="Endgame EPA" value={overall.epa.breakdown.endgame_points} />
+                        </InfoRow>
+                    </>
                 )}
+
                 <Text
                     style={{
                         fontWeight: "bold",
                         fontSize: 15,
-                        marginTop: 10,
+                        marginTop: 16,
                         color: colors.text,
                     }}
                 >
-                    Past Competition Stats
+                    Past Competitions
                 </Text>
-                <ScrollView>
-                    {overall &&
-                        "epa" in overall &&
-                        competitions != null &&
-                        competitions[0] != null &&
-                        competitions.map((comp) => (
-                            <View style={{ paddingVertical: 10 }}>
-                                <Text style={{ fontWeight: "500", color: colors.text }}>{comp.event_name}</Text>
+                <FlatList
+                    style={{ marginTop: 6 }}
+                    data={competitions}
+                    keyExtractor={(item) => item.event_key}
+                    ListEmptyComponent={() => (
+                        <Text style={{ color: colors.text, fontStyle: "italic" }}>No competitions found.</Text>
+                    )}
+                    ItemSeparatorComponent={() => <View style={{ height: 8 }}></View>}
+                    renderItem={({ item }) => (
+                        <View>
+                            <Text style={{ fontWeight: "500", color: colors.text }}>{item.event_name}</Text>
+                            {item.epa.breakdown && (
                                 <InfoRow>
-                                    <InfoCapsule
-                                        title="Auto EPA"
-                                        value={comp.epa.breakdown.auto_points}
-                                        textColor={colors.text}
-                                    />
-                                    <InfoCapsule
-                                        title="Teleop EPA"
-                                        value={comp.epa.breakdown.teleop_points}
-                                        textColor={colors.text}
-                                    />
-                                    <InfoCapsule
-                                        title="Endgame EPA"
-                                        value={comp.epa.breakdown.endgame_points}
-                                        textColor={colors.text}
-                                    />
+                                    <InfoCapsule title="Auto EPA" value={item.epa.breakdown.auto_points} />
+                                    <InfoCapsule title="Teleop EPA" value={item.epa.breakdown.teleop_points} />
+                                    <InfoCapsule title="Endgame EPA" value={item.epa.breakdown.endgame_points} />
                                 </InfoRow>
-                            </View>
-                        ))}
-                </ScrollView>
-                <Text style={{ textAlign: "center", color: "gray", marginLeft: "2%" }}>Powered by Statbotics</Text>
+                            )}
+                        </View>
+                    )}
+                />
+                <Text style={{ marginTop: 16, textAlign: "center", color: "gray" }}>Powered by Statbotics</Text>
             </View>
-        </View>
-    );
+        );
+    }
 }
